223f36175ca78fc596ac77c851bdd322019f56ad
Smirnov Mikhael <smirnov_mikhael@bk.ru>
2024-01-11 14:24:37 +0300

    Добавили мсправления

--- HW-2/src/Main.java
+++ HW-2/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Главный класс с методом main для примера использования Pair и GenericMethods.
  */
@@ -7,11 +9,31 @@ public class Main {
      * @param args Аргументы командной строки (не используются).
      */
     public static void main(String[] args) {
-        // Пример использования
-        Pair<Integer> intPair = new Pair<>(2, 3);
-        System.out.println("Sum: " + GenericMethods.sum(intPair));
+        Scanner scanner = new Scanner(System.in);
 
-        Pair<String> stringPair = new Pair<>("Hello, ", "world!");
-        System.out.println("Concatenation: " + GenericMethods.concatenate(stringPair));
+        // Введите два числа
+        System.out.print("Введите первое число: ");
+        int firstNumber = scanner.nextInt();
+
+        System.out.print("Введите второе число: ");
+        int secondNumber = scanner.nextInt();
+
+        // Создайте пару чисел
+        Pair<Integer> intPair = new Pair<>(firstNumber, secondNumber);
+        System.out.println("Сумма чисел: " + GenericMethods.sum(intPair));
+
+        // Введите две строки
+        System.out.print("Введите первую строку: ");
+        String firstString = scanner.next();
+
+        System.out.print("Введите вторую строку: ");
+        String secondString = scanner.next();
+
+        // Создайте пару строк
+        Pair<String> stringPair = new Pair<>(firstString, secondString);
+        System.out.println("Конкатенация строк: " + GenericMethods.concatenate(stringPair));
+
+        // Закрыть Scanner
+        scanner.close();
     }
 }
