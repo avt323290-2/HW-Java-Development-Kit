f8dfa8f6c828062ef6735e39b632f2b4569c9ab3
Smirnov Mihail <smirnov_mikhael@bk.ru>
2024-01-23 09:59:56 +0300

    Закончили код программы задания №5

--- HW_5/src/DiningTable.java
+++ HW_5/src/DiningTable.java
@@ -1,2 +1,19 @@
+/**
+ * Класс, представляющий обеденный стол с философами и вилками.
+ */
 public class DiningTable {
+    public static void main(String[] args) {
+        int numPhilosophers = 5;
+        Philosopher[] philosophers = new Philosopher[numPhilosophers];
+        Fork[] forks = new Fork[numPhilosophers];
+
+        for (int i = 0; i < numPhilosophers; i++) {
+            forks[i] = new Fork();
+        }
+
+        for (int i = 0; i < numPhilosophers; i++) {
+            philosophers[i] = new Philosopher(i, forks[i], forks[(i + 1) % numPhilosophers]);
+            philosophers[i].start();
+        }
+    }
 }
--- HW_5/src/Philosopher.java
+++ HW_5/src/Philosopher.java
@@ -1,2 +1,41 @@
-public class Philosopher {
+/**
+ * Класс, представляющий философа за обеденным столом.
+ */
+public class Philosopher extends Thread {
+    private int id;
+    private Fork leftFork;
+    private Fork rightFork;
+
+    /**
+     * Конструктор для создания философа с указанным ID и вилками.
+     *
+     * @param id         ID философа.
+     * @param leftFork   Левая вилка.
+     * @param rightFork  Правая вилка.
+     */
+    public Philosopher(int id, Fork leftFork, Fork rightFork) {
+        this.id = id;
+        this.leftFork = leftFork;
+        this.rightFork = rightFork;
+    }
+
+    private void eat() {
+        System.out.println("Философ " + id + " обедает.");
+    }
+
+    private void think() {
+        System.out.println("Философ " + id + " размышляет.");
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 3; i++) {
+            think();
+            leftFork.pickUp();
+            rightFork.pickUp();
+            eat();
+            leftFork.putDown();
+            rightFork.putDown();
+        }
+    }
 }
